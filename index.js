7b7d0630392e9bc4f6972b97b51354c819b0d0e1
sultangithub04 <sult_04@yahoo.com>
2024-12-24 21:00:06 +0600

    Build APIs for Order Management

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ async function run() {
             const result = await foodCollection.findOne(query)
             res.send(result)
         })
+        app.get('/food/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await foodCollection.findOne(query)
+            res.send(result)
+        })
         // food count
         app.get('/foodCount', async (req, res) => {
             count = await foodCollection.estimatedDocumentCount();
